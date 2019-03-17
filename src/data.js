80505d87f3ccb6a00503f024d7400c8b5b158a2a
HuskyHsu <chessman416@hotmail.com>
2019-03-17 08:20:17 +0800

    修正ajax

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ const host = 'https://api.imgur.com/3';
 const get = async function () {
 
     const path = '/album/y9yElNB/images'
-    const response = await fetch(`${host}${path}`, {
+    const response = await fetch(`${host}${path}?_=${new Date().getTime()}`, {
         method: 'GET',
         headers: {
             'Authorization': `Client-ID cb0681a079ca451`
@@ -28,7 +28,7 @@ const get = async function () {
 const post = async function (formData) {
 
     const path = '/image'
-    const response = await fetch(`${host}${path}&_=${new Date().getTime()}`, {
+    const response = await fetch(`${host}${path}`, {
         method: 'POST',
         headers: {
             'Authorization': `Bearer c1a0b11db42b730983644bf7087d1aa031b6c415`
